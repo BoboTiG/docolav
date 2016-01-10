f41b6f445d78bdca8415a02ba21901fc8d3c6436
BoboTiG <bobotig@gmail.com>
2016-01-10 10:23:40 +0100

    Version 0.3.3

--- src/maj-cimg.sh
+++ src/maj-cimg.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-[ -f CImg.h ] && mv CImg.h CImg.h_$(date "+%s")
+[ -f CImg.h ] && rm CImg.h
 wget -c https://raw.githubusercontent.com/dtschump/CImg/master/CImg.h -O - | gunzip - > CImg.h
